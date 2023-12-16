34b19bcd038534e56327a1e5d0835f33a51e56e4
Akash <142705218+Akashtronics@users.noreply.github.com>
2023-12-17 02:42:36 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,1 +1,58 @@
+# This is a Python program for a simple calculator
 
+# Function to add two numbers
+def add(num1, num2):
+	return num1 + num2
+
+# Function to subtract two numbers
+def subtract(num1, num2):
+	return num1 - num2
+
+# Function to multiply two numbers
+def multiply(num1, num2):
+	return num1 * num2
+
+# Function to divide two numbers
+def divide(num1, num2):
+	return num1 / num2
+
+# Function to find one to the power another number
+def power(num1, num2):
+  return num1 ** num2
+  
+print("Please select operation -\n" \
+		"1. Add\n" \
+		"2. Subtract\n" \
+		"3. Multiply\n" \
+		"4. Divide\n" \
+    "5. Power\n")
+
+
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4, 5:"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+if select == 1:
+	print(number_1, "+", number_2, "=",
+					add(number_1, number_2))
+
+elif select == 2:
+	print(number_1, "-", number_2, "=",
+					subtract(number_1, number_2))
+
+elif select == 3:
+	print(number_1, "*", number_2, "=",
+					multiply(number_1, number_2))
+
+elif select == 4:
+	print(number_1, "/", number_2, "=",
+					divide(number_1, number_2))
+  
+elif select == 5:
+  print(number_1, "**", number_2, "=",
+					power(number_1, number_2))
+  
+else:
+	print("Invalid input")
