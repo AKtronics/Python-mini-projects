3760083f34766a5a37144e8277f66ad8e1b3cc95
Akash <142705218+Akashtronics@users.noreply.github.com>
2023-12-17 13:18:34 +0530

    Update Palindrome_check.py

--- Palindrome_check.py
+++ Palindrome_check.py
@@ -1,1 +1,19 @@
+#This is a python program for checking if an array is a palindrome or not.
+
+#A palindrome is a sequence which when reversed is same as the original one.
+
+def Palindrome(arr) :
+    rra = arr[ : :-1]
+    
+    if arr == rra :
+      print("YES the array is a palindrome ")
+    else :
+      print("NO the array is not a palindrome")
+      
+      
+#Test cases
+      
+Palindrome("121")
+Palindrome((4,5,4))
+Palindrome ([1,2,3])
 
