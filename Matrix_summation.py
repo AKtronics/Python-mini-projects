121b7b53e9a87c05af14de1650124420adc47e6e
Akash <142705218+Akashtronics@users.noreply.github.com>
2023-12-19 16:17:00 +0530

    Update Matrix_summation.py

--- Matrix_summation.py
+++ Matrix_summation.py
@@ -1,20 +1,27 @@
 # This is a python program for adding two matrices.
+
 # Function to add two matrices
+
 def add_matrices(mat1, mat2):
+ 
  # Check if the matrices have the same dimensions
+ 
  if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
- return "Matrices must have the same dimensions for addition"
+  return "Matrices must have the same dimensions for addition"
  
  # Initialize an empty result matrix with the same dimensions
+ 
  result = []
  for i in range(len(mat1)):
- row = []
- for j in range(len(mat1[0])):
- row.append(mat1[i][j] + mat2[i][j])
- result.append(row)
+  row = []
+  for j in range(len(mat1[0])):
+   row.append(mat1[i][j] + mat2[i][j])
+   result.append(row)
  
  return result
+
 # Input matrices
+
 matrix1 = [
  [1, 2, 3],
  [4, 5, 6],
@@ -25,12 +32,16 @@ matrix2 = [
  [6, 5, 4],
  [3, 2, 1]
 ]
+
 # Call the add_matrices function
+
 result_matrix = add_matrices(matrix1, matrix2)
+#@Aktronics ;)
 # Display the result
+
 if isinstance(result_matrix, str):
  print(result_matrix)
 else:
  print("Sum of matrices:")
  for row in result_matrix:
- print(row)
+  print(row)
